107ed46bfad3ce8b051a0462d08f8a98b1450cf0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-12-27 14:57:29 +0000

    Reformatted with black

--- 03_execution/flush.py
+++ 03_execution/flush.py
@@ -14,5 +14,5 @@ def main():
     print("Done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- 03_execution/turnon.py
+++ 03_execution/turnon.py
@@ -19,5 +19,5 @@ def main():
     print("Done")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
